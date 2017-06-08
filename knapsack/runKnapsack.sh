3b760ef5175977adf3498b8ba928f1c421651b88
Kevin Do <kdo042@Zeta.coe.neu.edu>
2017-06-07 21:44:13 -0400

    ran all instances through sh script

---
+++ knapsack/runKnapsack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ampl runner/knapsack8.run > output/knapsack8.output
+ampl runner/knapsack12.run > output/knapsack12.output
+ampl runner/knapsack16.run > output/knapsack16.output
+ampl runner/knapsack20.run > output/knapsack20.output
+ampl runner/knapsack28.run > output/knapsack28.output
+ampl runner/knapsack32.run > output/knapsack32.output
+ampl runner/knapsack48.run > output/knapsack48.output
+ampl runner/knapsack64.run > output/knapsack64.output
+ampl runner/knapsack128.run > output/knapsack128.output
+ampl runner/knapsack256.run > output/knapsack256.output
+ampl runner/knapsack512.run > output/knapsack512.output
+ampl runner/knapsack1024.run > output/knapsack1024.output
